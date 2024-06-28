56461a8a5cf4b51ffccd39df86fbeb9b49d899dd
Ariel <ariel.nicolas.aguilar@gmail.com>
2024-06-28 19:14:57 -0300

    Add new proyects

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,8 @@ let arrowRight = document.getElementById('arrowRight')
 let pagOne = document.getElementById('pagOne')
 let pagTwo = document.getElementById('pagTwo')
 let pagThree = document.getElementById('pagThree')
+let pagFour = document.getElementById('pagFour')
+let pagFive = document.getElementById('pagFive')
 let index = 1
 
 arrowRight.addEventListener('click', () => {
@@ -22,6 +24,8 @@ arrowRight.addEventListener('click', () => {
             pagOne.classList.remove('active')
             pagTwo.classList.add('active')
             pagThree.classList.remove('active')
+            pagFour.classList.remove('active')
+            pagFive.classList.remove('active')
             break
         case 2:
             carrousel.style.transform = "translateX(-200%)"
@@ -30,14 +34,38 @@ arrowRight.addEventListener('click', () => {
             pagOne.classList.remove('active')
             pagTwo.classList.remove('active')
             pagThree.classList.add('active')
+            pagFour.classList.remove('active')
+            pagFive.classList.remove('active')
             break
         case 3:
+            carrousel.style.transform = "translateX(-300%)"
+            index = 4
+
+            pagOne.classList.remove('active')
+            pagTwo.classList.remove('active')
+            pagThree.classList.remove('active')
+            pagFour.classList.add('active')
+            pagFive.classList.remove('active')
+            break
+        case 4:
+            carrousel.style.transform = 'translate(-400%)'
+            index = 5
+
+            pagOne.classList.remove('active')
+            pagTwo.classList.remove('active')
+            pagThree.classList.remove('active')
+            pagFour.classList.remove('active')
+            pagFive.classList.add('active')
+            break
+        case 5:
             carrousel.style.transform = "translateX(0%)"
             index = 1
 
             pagOne.classList.add('active')
             pagTwo.classList.remove('active')
             pagThree.classList.remove('active')
+            pagFour.classList.remove('active')
+            pagFive.classList.remove('active')
             break
         default:
             break
@@ -47,12 +75,14 @@ arrowRight.addEventListener('click', () => {
 arrowLeft.addEventListener('click', () => {
     switch (index) {
         case 1:
-            carrousel.style.transform = 'translateX(-200%)'
-            index = 3
+            carrousel.style.transform = 'translateX(-400%)'
+            index = 5
 
             pagOne.classList.remove('active')
             pagTwo.classList.remove('active')
-            pagThree.classList.add('active')
+            pagThree.classList.remove('active')
+            pagFour.classList.remove('active')
+            pagFive.classList.remove('active')
             break
         case 2:
             carrousel.style.transform = 'translateX(0%)'
@@ -61,6 +91,8 @@ arrowLeft.addEventListener('click', () => {
             pagOne.classList.add('active')
             pagTwo.classList.remove('active')
             pagThree.classList.remove('active')
+            pagFour.classList.remove('active')
+            pagFive.classList.remove('active')
             break
         case 3:
             carrousel.style.transform = 'translateX(-100%)'
@@ -69,6 +101,28 @@ arrowLeft.addEventListener('click', () => {
             pagOne.classList.remove('active')
             pagTwo.classList.add('active')
             pagThree.classList.remove('active')
+            pagFour.classList.remove('active')
+            pagFive.classList.remove('active')
+            break
+        case 4:
+            carrousel.style.transform = 'translateX(-200%)'
+            index = 3
+
+            pagOne.classList.remove('active')
+            pagTwo.classList.remove('active')
+            pagThree.classList.add('active')
+            pagFour.classList.remove('active')
+            pagFive.classList.remove('active')
+            break
+        case 5:
+            carrousel.style.transform = 'translateX(-300%)'
+            index = 4
+
+            pagOne.classList.remove('active')
+            pagTwo.classList.remove('active')
+            pagThree.classList.remove('active')
+            pagFour.classList.add('active')
+            pagFive.classList.remove('active')
             break
         default:
             break
