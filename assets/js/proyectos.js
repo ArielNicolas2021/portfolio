0556547ed342b887ef7bdeef4b534e1a5e2e621b
Ariel <ariel.nicolas.aguilar@gmail.com>
2024-09-10 11:19:51 -0300

    Eliminate proyectos

--- assets/js/proyectos.js
+++
@@ -1,6 +0,0 @@
-let menuBtn = document.getElementById('menuBtn')
-let navbar = document.getElementById('navbar')
-
-menuBtn.addEventListener('click', () => {
-    navbar.classList.toggle('show')
-})
